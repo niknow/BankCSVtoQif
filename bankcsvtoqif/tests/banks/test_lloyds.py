9060399142363a2225d8c2759e7a20d35e1140f5
Nikolai <mail@nikno.de>
2015-09-09 23:14:18 +0100

    added unittest for lloyds

---
+++ bankcsvtoqif/tests/banks/test_lloyds.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+
+# BankCSVtoQif - Smart conversion of csv files from a bank to qif
+# Copyright (C) 2015  Nikolai Nowaczyk
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import unittest
+from datetime import datetime
+from bankcsvtoqif.tests.banks import csvline_to_line
+
+from bankcsvtoqif.banks.lloyds import Lloyds
+
+
+class TestLLoyds(unittest.TestCase):
+
+    def setUp(self):
+        self.csv = """20/07/2015,DEB,'12-34-56,12345678,GROCE RIES CD 1234 ,12.34,,1234.56"""
+
+    def test_can_instantiate(self):
+        account_config = Lloyds()
+        self.assertEqual(type(account_config), Lloyds)
+
+    def test_getters(self):
+        account_config = Lloyds()
+        line = csvline_to_line(self.csv, account_config)
+        date = datetime(2015, 7, 20)
+        description = 'GROCE RIES CD 1234 '
+        debit = 12.34
+        credit = 0
+        self.assertEqual(account_config.get_date(line), date)
+        self.assertEqual(account_config.get_description(line), description)
+        self.assertEqual(account_config.get_debit(line), debit)
+        self.assertEqual(account_config.get_credit(line), credit)
